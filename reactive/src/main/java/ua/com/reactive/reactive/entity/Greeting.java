9a85f0b96cf8c4d37c52a2479134a89412f9e270
LETBOUNCE\letbounce <olegkoksiuk@gmail.com>
2025-10-05 13:49:50 +0300

    added new classes and adjusted localhost to 2 Lab requirements

---
+++ reactive/src/main/java/ua/com/reactive/reactive/entity/Greeting.java
@@ -0,0 +1,13 @@
+package ua.com.reactive.reactive.entity;
+
+public class Greeting {
+    private final String message;
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
